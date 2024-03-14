0c7478c6503d5e7b9a944143890c2776368f9565
ABSOLUTEbeginnerr <82446276+ABSOLUTEbeginnerr@users.noreply.github.com>
2024-03-14 04:54:58 +0200

    project 1
    project 1

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 // index.js
 // where your node app starts
 
@@ -27,6 +28,6 @@ app.get("/api/hello", function (req, res) {
 
 
 // Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env['PORT'] || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
